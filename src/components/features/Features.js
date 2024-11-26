2eb7a5d7b27213e6161c03f53923a6e74133613b
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-11-26 17:44:18 +0000

    Refactor Features component to update card titles and descriptions

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { AiFillAppstore } from "react-icons/ai";
-import { FaMicrophone, FaPodcast, FaComments, FaBullhorn, FaPenNib, FaUsers } from "react-icons/fa";
+import { FaMicrophone, FaPodcast, FaComments, FaBullhorn, FaPenNib, FaUsers, FaLaptop, FaChartLine } from "react-icons/fa";
 import Title from '../layouts/Title';
 import Card from './Card';
 
@@ -13,9 +12,9 @@ const Features = () => {
       <Title title="Features" des="What I Do" />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         <Card
-          title="Master of Ceremonies (MC)"
+          title="MCing and Moderation"
           des="Providing professional and engaging hosting services for your events, 
-          ensuring a smooth and memorable experience for all attendees."
+          ensuring a smooth and memorable experience for all attendees. Facilitating discussions that bring out the best in panelists, ensuring a balanced and productive dialogue that keeps the audience engaged."
           icon={<FaMicrophone />}
         />
         <Card
@@ -23,12 +22,6 @@ const Features = () => {
           des="Hosting thoughtful and engaging podcasts on relevant social issues, 
           bringing insightful conversations and diverse perspectives to the forefront."
           icon={<FaPodcast />}
-        />
-        <Card
-          title="Moderation"
-          des="Facilitating discussions that bring out the best in panelists, 
-          ensuring a balanced and productive dialogue that keeps the audience engaged."
-          icon={<FaComments />}
         />
         <Card
           title="Voice Overs"
@@ -47,6 +40,18 @@ const Features = () => {
           des="Delivering powerful and inspiring speeches for any audience, 
           motivating and empowering listeners with compelling storytelling and impactful messages."
           icon={<FaUsers />}
+        />
+        <Card
+          title="Freelance Writing"
+          des="Creating engaging and well-researched content for various platforms, 
+          including blogs, articles, and social media, tailored to meet the needs of clients."
+          icon={<FaLaptop />}
+        />
+        <Card
+          title="Digital Marketing"
+          des="Developing and implementing effective digital marketing strategies, 
+          including SEO, social media marketing, and content marketing, to enhance online presence and drive engagement."
+          icon={<FaChartLine />}
         />
       </div>
     </section>
