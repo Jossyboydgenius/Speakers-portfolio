ad6dcba1d79984ac082bda6be5e3a5ec38de9616
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-11-28 15:34:54 +0000

    Refactor Volunteering component to update section title

--- src/components/resume/Volunteering.js
+++ src/components/resume/Volunteering.js
@@ -43,7 +43,7 @@ const Volunteering = () => {
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2020 - 2023</p>
-          <h2 className="text-3xl md:text-4xl font-bold">Other Roles</h2>
+          <h2 className="text-3xl md:text-4xl font-bold">Volunteering Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
