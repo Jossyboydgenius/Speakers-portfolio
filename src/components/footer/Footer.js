f197ad7d744a819f21746fa4789e9bb0e5f56419
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-11-28 16:59:47 +0000

    Refactor Footer component to comment out the "Blog" link temporarily

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -76,12 +76,12 @@ const Footer = () => {
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </Link>
           </li>
-          <li>
+          {/* <li>
             <Link to="blog" smooth duration={500} className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
               Blog
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </Link>
-          </li>
+          </li> */}
         </ul>
       </div>
 
