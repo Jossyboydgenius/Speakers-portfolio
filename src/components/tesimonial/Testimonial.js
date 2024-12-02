0b2dd88991c377e548c4a7db42d6e2b39d104744
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-12-02 23:23:13 +0000

    Refactor Testimonial component to update testimonial dot color

--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -71,7 +71,7 @@ const Testimonial = () => {
                    width: "12px",
                    height: "12px",
                    color: "blue",
-                   background: "#ff014f",
+                   background: "#be8c6c",
                    borderRadius: "50%",
                    cursor: "pointer",
                  }
