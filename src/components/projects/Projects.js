6947aaad2ddf9cfcf4d802d15b7709797e84ce63
Joseph Awe <jossyboydgenius@gmail.com>
2026-06-21 00:30:42 +0000

    feat: implement portfolio structure, project showcase components, and resume sections with new dependencies.

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectTwo, projectThree, projectFour, projectFive, projectSix, projectNine } from "../../assets/index";
+import { projectOne, projectTwo, projectThree, projectFour, projectSix, projectNine } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
   return (
     <section
       id="projects"
-      className="w-full py-20 border-b-[1px] border-b-black"
+      className="w-full py-20 border-b border-white/5 font-bodyFont"
     >
       <div className="flex justify-center items-center text-center">
         <Title
@@ -15,7 +15,7 @@ const Projects = () => {
           des="My Projects"
         />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-8">
         <ProjectsCard
           title="Get Out of Depression (GOOD) Foundation"
           des="Led strategic development and digital marketing campaigns for the foundation, 
